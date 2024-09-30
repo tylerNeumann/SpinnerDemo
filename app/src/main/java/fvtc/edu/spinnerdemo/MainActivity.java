d73af069df43cf51eca033ddd7e10de31ce4b2e9
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2024-09-29 19:25:23 -0500

    added functionality

--- app/src/main/java/fvtc/edu/spinnerdemo/MainActivity.java
+++ app/src/main/java/fvtc/edu/spinnerdemo/MainActivity.java
@@ -1,6 +1,12 @@
 package fvtc.edu.spinnerdemo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = "MainActivity";
+    private static final String[] items ={
+            "Call of Duty",
+            "Counter-strike",
+            "Battlefield 1942",
+            "Hogwarts Legacy",
+            "World of Warcraft",
+            "Baldurs Gate",
+            "Icewind Dale",
+            "Palworld",
+            "Warframe"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        
+        initSpinner();
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    private void initSpinner() {
+        Spinner spItems = findViewById(R.id.spinner);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                items);
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spItems.setAdapter(arrayAdapter);
+
+        //handle clicking
+        spItems.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: ");
+                TextView tvInfo = findViewById(R.id.tvInfo);
+                tvInfo.setText(items[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 }
